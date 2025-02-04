f38989223fe8b3eb525dbd3fe8a7c17e589f66e8
samarthsb4real <samarthbhadane23@gmail.com>
2025-02-04 14:09:22 +0530

    add constructor and createDeck method to Deck class

--- Deck.java
+++ Deck.java
@@ -3,5 +3,15 @@ import java.util.*;
 
 class Deck {
     ArrayList<Card> deck;
-    
+
+    // Constructor
+    public Deck() {
+        this.deck = new ArrayList<>();
+    }
+
+    // Create a deck of 52 cards
+    public void createDeck() {
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+    }
 }
