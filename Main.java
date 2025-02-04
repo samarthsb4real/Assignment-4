42872f07afb3c18ce40ceefc77c0b4a828403b0b
samarthsb4real <samarthbhadane23@gmail.com>
2025-02-04 14:47:48 +0530

    refactor Main class to use selectCards method for selecting random cards

--- Main.java
+++ Main.java
@@ -4,14 +4,11 @@ class Main {
         System.out.println("Create the Deck of cards:");
         deck.createDeck();
         System.out.println("Display the Deck of cards:");
-        deck.displayDeck();
         System.out.println("Shuffle the Deck of cards:");
         deck.shuffleDeck();
 
         // Select 3 random cards from the deck
-        System.out.println("Select 3 cards from the Deck:");
-        for (int i = 0; i < 3; i++) {
-            System.out.println(deck.deck.get(i));
-        }
+        System.out.println("\nSelect 3 cards from the Deck:");
+        deck.selectCards(3);
     }
 }
