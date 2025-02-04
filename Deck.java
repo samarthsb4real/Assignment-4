e9aad06644171b903e77882c6f58b1e4712c423e
samarthsb4real <samarthbhadane23@gmail.com>
2025-02-04 14:13:36 +0530

    implement card creation in createDeck method of Deck class

--- Deck.java
+++ Deck.java
@@ -13,5 +13,13 @@ class Deck {
     public void createDeck() {
         String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
         String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+
+        // For each suit, create a card for each rank
+        for (String rank : ranks) {
+            for (String suit : suits) {
+                Card card = new Card(rank, suit);
+                deck.add(card);
+            }
+        }
     }
 }
