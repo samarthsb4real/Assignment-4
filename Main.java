5cb705d901299b1027f858bbddf04131086d0d9e
samarthsb4real <samarthbhadane23@gmail.com>
2025-02-04 14:24:07 +0530

    add output messages to Main class for deck creation and display

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
-import java.util.*;
-
 class Main{
     public static void main(String[] args) {
         Deck deck = new Deck();
+        System.out.println("Create the Deck of cards:");
         deck.createDeck();
+        System.out.println("Display the Deck of cards:");
         deck.displayDeck();
     }
 }
