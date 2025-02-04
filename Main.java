5c6e645678d46b4d12788568a8c38c29b3838027
samarthsb4real <samarthbhadane23@gmail.com>
2025-02-04 14:39:15 +0530

    add shuffleDeck call and select random cards in Main class

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
-class Main{
+class Main {
     public static void main(String[] args) {
         Deck deck = new Deck();
         System.out.println("Create the Deck of cards:");
         deck.createDeck();
         System.out.println("Display the Deck of cards:");
         deck.displayDeck();
+        System.out.println("Shuffle the Deck of cards:");
+        deck.shuffleDeck();
+
+        // Select 3 random cards from the deck
+        System.out.println("Select 3 cards from the Deck:");
+        for (int i = 0; i < 3; i++) {
+            System.out.println(deck.deck.get(i));
+        }
     }
 }
